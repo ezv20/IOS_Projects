94ad11ac7d5aa57bf262f5df493d0ebcf38a521e
Edwin Zepeda <edwin.zpd@gmail.com>
2022-09-22 23:27:58 -0400

    Adding folder for first time

---
+++ Swift UI/SwiftUI_WeSplit/SwiftUI_WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_WeSplit
+//
+//  Created by Edwin Zepeda on 9/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift UI/SwiftUI_WeSplit/SwiftUI_WeSplit/SwiftUI_WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeSplitApp.swift
+//  SwiftUI_WeSplit
+//
+//  Created by Edwin Zepeda on 9/21/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
